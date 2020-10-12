1746eca581f2bdcbd6b8542f6be0e7e8790cf452
Craig Heydenburg <craigh@mac.com>
2020-10-12 05:59:51 -0400

    refactor to runtime implementations (#4522)
    * refactor Blocks and DefaultTheme twig extensions to runtime implementations

--- Twig/Extension/OnlineExtension.php
+++ Twig/Extension/OnlineExtension.php
@@ -13,61 +13,16 @@ declare(strict_types=1);
 
 namespace Zikula\UsersModule\Twig\Extension;
 
-use DateTime;
-use Doctrine\Common\Collections\Criteria;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
-use Zikula\ExtensionsModule\Api\ApiInterface\VariableApiInterface;
-use Zikula\SecurityCenterModule\Constant;
-use Zikula\UsersModule\Entity\RepositoryInterface\UserSessionRepositoryInterface;
-use Zikula\UsersModule\Entity\UserEntity;
+use Zikula\UsersModule\Twig\Runtime\OnlineRuntime;
 
 class OnlineExtension extends AbstractExtension
 {
-    /**
-     * @var UserSessionRepositoryInterface
-     */
-    private $sessionRepository;
-
-    /**
-     * @var string
-     */
-    private $sessionStorageInFile;
-
-    public function __construct(
-        UserSessionRepositoryInterface $sessionRepository,
-        VariableApiInterface $variableApi
-    ) {
-        $this->sessionRepository = $sessionRepository;
-        $this->sessionStorageInFile = $variableApi->getSystemVar('sessionstoretofile', 1);
-    }
-
     public function getFilters()
     {
         return [
-            new TwigFilter('onlineSince', [$this, 'onlineSince'])
+            new TwigFilter('onlineSince', [OnlineRuntime::class, 'onlineSince'])
         ];
-    }
-
-    /**
-     * @return bool|void
-     */
-    public function onlineSince(UserEntity $userEntity = null, int $minutes = 10)
-    {
-        if (null === $userEntity) {
-            return;
-        }
-        if (Constant::SESSION_STORAGE_FILE === $this->sessionStorageInFile) {
-            return;
-        }
-
-        $since = new DateTime();
-        $since->modify("-${minutes} minutes");
-        $c = Criteria::create()
-            ->where(Criteria::expr()->eq('uid', $userEntity->getUid()))
-            ->andWhere(Criteria::expr()->gt('lastused', $since));
-        $online = $this->sessionRepository->matching($c)->count();
-
-        return (bool) $online;
     }
 }
